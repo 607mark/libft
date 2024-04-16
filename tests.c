309e55b8790f91e39965a1791ec3518b1c62e284
Mark Shabanov <mshabano@c3r4p3.hive.fi>
2024-04-16 15:48:42 +0300

    1st

---
+++ tests.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlen(const char *s);
+
+int main()
+{
+		char *s = "LOL";
+		int a = strlen(c);
+		int b = ft_strlen(c);
+		printf("%d - %d\n", a, b);
+}
