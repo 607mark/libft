b7308d67c61d860140bbf96cf0da5b05dc9786a8
Mark Shabanov <mshabano@c3r1p5.hive.fi>
2024-04-18 18:38:35 +0300

    few more

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mshabano <mshabano@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:54:08 by mshabano          #+#    #+#             */
-/*   Updated: 2024/04/18 16:46:36 by mshabano         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:11:05 by mshabano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t		dst_len;
-	size_t		len;
-	const char	*s;
-	
-	if ((!src || !dst) && (!dstsize)
-			return (0);
+	size_t	src_len;
+	size_t	dst_len;
+
+	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	if (dst_len >= dstsize)
-		return(
-
-
+	if (dst_len > dstsize)
+		dst_len = dstsize;
+	if (dst_len == dstsize)
+		return (dst_len + src_len);
+	if (src_len < dstsize - dst_len)
+		ft_memcpy(dst + dst_len, src, src_len + 1);
+	else
+	{
+		ft_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+		*(dst + dstsize - 1) = '\0';
+	}
+	return (dst_len + src_len);
 }
