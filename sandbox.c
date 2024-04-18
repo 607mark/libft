f72a6cec0f2ca3cff9e037a94c481c0f59934d82
Mark Shabanov <mshabano@c3r1p5.hive.fi>
2024-04-18 19:35:26 +0300

    a

--- sandbox.c
+++ sandbox.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-	char dst[20] = "Hello ";
-	char *src = "world";
-	size_t c = strlcat(dst, src, 2);
-	printf("%zu\n%s", c, dst);
+	int a = -128;
+	while(a < 128)
+	{
+		printf("%c", a++);
+	}
 }
