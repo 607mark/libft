c1d419f51952a801cf0905ac877c98b3edeafbec
Mark Shabanov <mshabano@c1r6p6.hive.fi>
2024-04-28 21:06:02 +0300

    a

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,11 @@
 /*   By: mshabano <mshabano@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:11:30 by mshabano          #+#    #+#             */
-/*   Updated: 2024/04/28 17:33:50 by mshabano         ###   ########.fr       */
+/*   Updated: 2024/04/28 20:45:03 by mshabano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
-
+/*
 static size_t	count_words(char const *s, char c)
 {
 	size_t	n;
@@ -66,11 +65,99 @@ char	**ft_split(char const *s, char c)
 			w_len = ft_strlen(s);
 		else
 			w_len = ft_strchr(s, c) - s;
-		arr[i++] = ft_substr(s, 0, w_len);
-		if (!(arr + i))
+		arr[i] = ft_substr(s, 0, w_len);
+		i++;
+		if ((arr + i) == 0)
 			return (free_arr(arr, i));
 		s += w_len;
 	}
 	arr[i] = 0;
 	return (arr);
+}
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	check_sep(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i] != '\0')
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	if (c == '\0')
+		return (1);
+	return (0);
+}
+
+int	total_words(char *str, char *charset)
+{
+	int	i;
+	int	words;
+
+	words = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (check_sep(str[i + 1], charset) == 1
+			&& check_sep(str[i], charset) == 0)
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+void	copy_word(char *dest, char *src, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (check_sep(src[i], charset) == 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+void	split_words(char **array, char *str, char *charset)
+{
+	int	i;
+	int	j;
+	int	word;
+
+	word = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (check_sep(str[i], charset) == 1)
+			i++;
+		else
+		{
+			j = 0;
+			while (check_sep(str[i + j], charset) == 0)
+				j++;
+			array[word] = (char *)malloc(sizeof(char) * (j + 1));
+			copy_word(array[word], str + i, charset);
+			i += j;
+			word++;
+		}
+	}
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**array;
+	int		words;
+
+	words = total_words(str, charset);
+	array = (char **)malloc(sizeof(char *) * (words + 1));
+	array[words] = 0;
+	split_words(array, str, charset);
+	return (array);
 }
